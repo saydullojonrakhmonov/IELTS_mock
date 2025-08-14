746bb6296887a34c6f04c218dab4d22a99bcd351
rahmonovsaydullo <saydullo779@gmail.com>
2025-08-14 11:44:25 +0500

    I changed localdb to server link

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,24 @@
-const {Pool} = require("pg")
+// const {Pool} = require("pg")
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "12345678",
-    port: 5433
-})
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "postgres",
+//     password: "12345678",
+//     port: 5433
+// })
+
+// module.exports = pool;
+
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: { rejectUnauthorized: false }
+});
 
 module.exports = pool;
